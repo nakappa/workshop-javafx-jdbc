8a955600ef9b2c1d69364d0936f7d7b93484c864
Inhanga <nakappa1@gmail.com>
2020-06-22 00:07:42 -0300

    Remove department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,6 +1,5 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -19,5 +18,9 @@ public class DepartmentService {
 		if (obj.getId() == null) dao.insert(obj);
 		
 		else dao.update(obj);
+	}
+	
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
 	}
 }
